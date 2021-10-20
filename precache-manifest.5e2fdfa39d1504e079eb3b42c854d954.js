94f295fbed8fa38e3544c5e6f8023af5772db19a
unknown <alextq3036@gmail.com>
2021-10-20 15:17:37 -0400

    Updates

---
+++ precache-manifest.5e2fdfa39d1504e079eb3b42c854d954.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e6d5ad612857540f1e13990db8863e1",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "e5834f40593aebe76362",
+    "url": "/markdown-previewer/static/css/main.b97a3200.chunk.css"
+  },
+  {
+    "revision": "6c40bfd95cecddf9796f",
+    "url": "/markdown-previewer/static/js/2.c21db458.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/markdown-previewer/static/js/2.c21db458.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e5834f40593aebe76362",
+    "url": "/markdown-previewer/static/js/main.8023b6af.chunk.js"
+  },
+  {
+    "revision": "7d7020aead3aeb8b702e",
+    "url": "/markdown-previewer/static/js/runtime-main.e3059d9b.js"
+  }
+]);
